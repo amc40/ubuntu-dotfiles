5af36ae28cace7f28c7733b6e04551dc2e0a45b4
Alan Chodyniecki <alan.chody@gmail.com>
2026-02-22 13:16:39 +0000

    Basic oh-my-zsh setup

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euxo pipefail
+
+USER_HOME="${USER_HOME:-$HOME}"
+DOTFILES_PATH="${DOTFILES_PATH:-$USER_HOME/ubuntu-dotfiles}"
+
+sudo apt update
+
+sudo apt install zsh -y
+
+if [ ! -d "$USER_HOME/.oh-my-zsh" ]
+then
+    echo "Installing oh-my-zsh"
+    CHSH=no KEEP_ZSHRC=yes RUNZSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+(cd $DOTFILES_PATH && cat .zshrc > "$USER_HOME/.zshrc")
+
+chsh -s $(which zsh) $(whoami)
+
